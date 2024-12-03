a236070c4f16a19530338503b2c0f77f6a761076
William Grant <williamg3575467@gmail.com>
2024-12-03 22:36:26 +0000

    Set body styles in index.html and begin landing/auth styling

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -1,32 +1,78 @@
 import { useState } from "react"
 import LoginForm from "./LoginForm"
 import Signup from "./SignupForm"
+import styled from "styled-components"
 
 
 const Index = () => {
   const [login, setLogin] = useState(false)
-  // Not final, just basic layout for now, although layout will change
+
   return (
-    <div>
+    <IndexWrapper>
       <h1>Welcome to chatapp</h1>
-      <div>
-        <div>
+      <ContentWrapper>
+        <Description>
           <p>Description goes here</p>
-        </div>
+        </Description>
         {
           login ?
-          <div>
-            <LoginForm />
-            <p>Not a member? <span onClick={() => { setLogin(!login) }}>Sign up</span></p>
-          </div> :
-          <div>
-            <Signup />
-            <p>Already have an account? <span onClick={() => { setLogin(!login) }}>Log in</span></p>
-          </div>
+          <FormWrapper>
+            <LoginForm setLogin={setLogin} loginState={login}/>
+          </FormWrapper> :
+          <FormWrapper>
+            <Signup setLogin={setLogin} loginState={login}/>
+          </FormWrapper>
         }
-      </div>
-    </div>
+      </ContentWrapper>
+    </IndexWrapper>
   )
 }
+
+const IndexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 40px;
+`
+
+const ContentWrapper = styled.div`
+  background-color: #a0a0a0;
+  margin-top: 20px;
+  display: flex;
+  gap: 25px;
+  height: 80vh;
+  border-radius: 10px;
+`
+
+const Description = styled.div`
+  border-radius: 10px;
+  background-color: #e0e0e0;
+  width: 100%;
+  margin: 35px;
+  margin-right: 0;
+  padding: 20px;
+
+  p {
+    margin: 0;
+    font-size: 1.2rem;
+  }
+`
+
+const FormWrapper = styled.div`
+  border-radius: 10px;
+  background-color: #e0e0e0;
+  width: 80%;
+  margin: 35px;
+  margin-left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+
+  p {
+    margin: 0;
+    font-size: 1.2rem;
+  }
+`
 
 export default Index
