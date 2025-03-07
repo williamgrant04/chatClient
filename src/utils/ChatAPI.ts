74f6015aa3ec3df329c1040c92455b091128f0b1
William Grant <williamg3575467@gmail.com>
2025-03-07 17:36:30 +0000

    Update functions so they aren't async promises

--- src/utils/ChatAPI.ts
+++ src/utils/ChatAPI.ts
@@ -3,263 +3,235 @@ const baseUrl = "http://localhost:3000"
 
 
 //* USER AUTH
-export const signup = (credentials: { email: string, username: string, password: string }) => {
-  return new Promise<User | string[]>(async (resolve, reject) => {
-    try {
-      const response = await axios.post(`${baseUrl}/signup`, {
-        user: {
-          ...credentials
-        }
-      })
+export const signup = async (credentials: { email: string, username: string, password: string }): Promise<User | string[]> => {
+  try {
+    const response = await axios.post(`${baseUrl}/signup`, {
+      user: {
+        ...credentials
+      }
+    })
 
-      console.log(response)
-      localStorage.setItem("token", response.headers.authorization.split(" ")[1])
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    localStorage.setItem("token", response.headers.authorization.split(" ")[1])
+    return response.data.user
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
-export const login = (credentials: { email: string, password: string }) => {
-  return new Promise<User | string[]>(async (resolve, reject) => {
-    try {
-      const response = await axios.post(`${baseUrl}/login`, {
-        user: {
-          ...credentials
-        }
-      })
+export const login = async (credentials: { email: string, password: string }): Promise<User | string[]> => {
+  try {
+    const response = await axios.post(`${baseUrl}/login`, {
+      user: {
+        ...credentials
+      }
+    })
 
-      console.log(response)
-      localStorage.setItem("token", response.headers.authorization.split(" ")[1])
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    localStorage.setItem("token", response.headers.authorization.split(" ")[1])
+    return response.data.user
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
-export const logout = () => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await axios.delete(`${baseUrl}/logout`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const logout = async () => {
+  try {
+    const response = await axios.delete(`${baseUrl}/logout`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      localStorage.removeItem("token")
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    localStorage.removeItem("token")
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
-export const loggedin = () => {
-  return new Promise<{ logged_in: boolean, user?: User }>(async (resolve, reject) => {
-    try {
-      const response = await axios.get(`${baseUrl}/loggedin`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const loggedin = async (): Promise<{ logged_in: boolean, user?: User }> => {
+  try {
+    const response = await axios.get(`${baseUrl}/loggedin`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
 //* SERVERS
-export const getServers = () => {
-  return new Promise<Server[]>(async (resolve, reject) => {
-    try {
-      const response = await axios.get(`${baseUrl}/servers`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const getServers = async (): Promise<Server[]> => {
+  try {
+    const response = await axios.get(`${baseUrl}/servers`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
-export const getServer = (id: string) => {
-  return new Promise<Server>(async (resolve, reject) => {
-    try {
-      const response = await axios.get(`${baseUrl}/server/${id}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const getServer = async (id: string): Promise<Server> => {
+  try {
+    const response = await axios.get(`${baseUrl}/server/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
-export const newServer = (name: string) => {
-  return new Promise<Server | {errors: string[]}>(async (resolve, reject) => {
-    try {
-      const response = await axios.post(`${baseUrl}/servers/new`, {
-        server: {
-          name
-        }
-      }, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const newServer = async (name: string): Promise<Server | {errors: string[]}> => {
+  try {
+    const response = await axios.post(`${baseUrl}/servers/new`, {
+      server: {
+        name
+      }
+    }, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
 //* CHANNELS
-export const getChannels = (serverId: string) => {
-  return new Promise<Channel[]>(async (resolve, reject) => {
-    try {
-      const response = await axios.get(`${baseUrl}/server/${serverId}/channels`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const getChannels = async (serverId: string): Promise<Channel[]> => {
+  try {
+    const response = await axios.get(`${baseUrl}/server/${serverId}/channels`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
-export const getChannel = (id: string) => {
-  return new Promise<Channel>(async (resolve, reject) => {
-    try {
-      const response = await axios.get(`${baseUrl}/channel/${id}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const getChannel = async (id: string) => {
+  try {
+    const response = await axios.get(`${baseUrl}/channel/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
-export const createChannel = (name: string, serverId: number) => {
-  return new Promise<Channel>(async (resolve, reject) => {
-    try {
-      const response = await axios.post(`${baseUrl}/server/${serverId}/channels/new`, {
-        channel: {
-          name
-        }
-      }, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const createChannel = async (name: string, serverId: number) => {
+  try {
+    const response = await axios.post(`${baseUrl}/server/${serverId}/channels/new`, {
+      channel: {
+        name
+      }
+    }, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
 //* MESSAGES
-export const getMessages = (channelId: string) => {
-  return new Promise<Message[]>(async (resolve, reject) => {
-    try {
-      const response = await axios.get(`${baseUrl}/channel/${channelId}/messages`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const getMessages = async (channelId: string) => {
+  try {
+    const response = await axios.get(`${baseUrl}/channel/${channelId}/messages`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(response.data)
-    } catch (error: any) {
-      reject(error.response.data)
-    }
-  })
+    console.log(response)
+    return response.data
+  } catch (error: any) {
+    return error.response.data
+  }
 }
 
 // This returns a boolean, because the message object comes from the subscription and not the API
-export const sendMessage = (content: string, channelId: string) => {
-  return new Promise<boolean>(async (resolve, reject) => {
-    try {
-      const response = await axios.post(`${baseUrl}/channel/${channelId}/messages/new`, {
-        message: {
-          content
-        }
-      }, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const sendMessage = async (content: string, channelId: string) => {
+  try {
+    const response = await axios.post(`${baseUrl}/channel/${channelId}/messages/new`, {
+      message: {
+        content
+      }
+    }, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(true)
-    } catch (error: any) {
-      reject(false)
-    }
-  })
+    console.log(response)
+    return true
+  } catch (_error: any) {
+    return false
+  }
 }
 
-export const editMessage = (content: string, messageId: number) => {
-  return new Promise<boolean>(async (resolve, reject) => {
-    try {
-      const response = await axios.patch(`${baseUrl}/message/${messageId}/edit`, {
-        message: {
-          content
-        }
-      }, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const editMessage = async (content: string, messageId: number) => {
+  try {
+    const response = await axios.patch(`${baseUrl}/message/${messageId}/edit`, {
+      message: {
+        content
+      }
+    }, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(true)
-    } catch (error: any) {
-      reject(false)
-    }
-  })
+    console.log(response)
+    return true
+  } catch (error: any) {
+    return false
+  }
 }
 
-export const deleteMessage = (messageId: number) => {
-  return new Promise<boolean>(async (resolve, reject) => {
-    try {
-      const response = await axios.delete(`${baseUrl}/message/${messageId}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-      })
+export const deleteMessage = async (messageId: number) => {
+  try {
+    const response = await axios.delete(`${baseUrl}/message/${messageId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
 
-      console.log(response)
-      resolve(true)
-    } catch (error: any) {
-      reject(false)
-    }
-  })
+    console.log(response)
+    return true
+  } catch (error: any) {
+    return false
+  }
 }
