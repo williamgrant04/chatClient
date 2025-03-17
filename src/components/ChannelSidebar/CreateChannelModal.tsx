148b592384a8be0cac2905f1856211bdb73d6bf7
William Grant <williamg3575467@gmail.com>
2025-03-17 12:31:42 +0000

    refactor: replace old modal with new modal component

--- src/components/ChannelSidebar/CreateChannelModal.tsx
+++ src/components/ChannelSidebar/CreateChannelModal.tsx
@@ -2,8 +2,7 @@ import ReactModal from "react-modal"
 import { createChannel } from "../../utils/ChatAPI"
 import styled from "styled-components"
 import { memo, useState } from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faXmarkCircle } from "@fortawesome/free-regular-svg-icons"
+import Modal from "../UI/Modal"
 
 const CreateChannelModal = memo(function CreateChannelModal({ setOpen, open, server }: { setOpen: React.Dispatch<React.SetStateAction<boolean>>, open: boolean, server: Server }) {
   ReactModal.setAppElement("#root")
@@ -31,56 +30,22 @@ const CreateChannelModal = memo(function CreateChannelModal({ setOpen, open, ser
     }
   }
 
-  const handleClose = () => {
-    setOpen(false)
-    setChannelName("")
-    setError("")
-  }
-
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setError("")
     setChannelName(e.target.value)
   }
 
   return (
-    <Modal isOpen={open} style={{ overlay: { backgroundColor: "rgb(0, 0, 0, 0.5)", zIndex: 3 } }} onRequestClose={() => setOpen(false)}>
-      <CloseButton onClick={handleClose}>
-        <FontAwesomeIcon icon={faXmarkCircle} />
-      </CloseButton>
+    <Modal open={open} setOpen={setOpen}>
       <h2>Create a new channel</h2>
       <ChannelForm onSubmit={handleChannelSubmit}>
         <ChannelInput type="text" placeholder="Channel name" name="channel" value={channelName} onChange={handleInputChange} />
-        <input type="submit" value="Create" id="newchannelsubmit" hidden/>
+        { error && <p>{error}</p> }
+        <CreateButton type="submit" value="Create" />
       </ChannelForm>
-      {error && <p>{error}</p>}
-      <CreateButton role="button" tabIndex={0} htmlFor="newchannelsubmit">Create</CreateButton>
     </Modal>
   )
 })
-
-const Modal = styled(ReactModal)`
-  outline: 0;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #fff;
-  border-radius: 10px;
-  padding: 20px;
-  width: 400px;
-  height: 200px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 3;
-
-  h2 {
-    margin: 0;
-    margin-bottom: 10px;
-  }
-`
 
 const ChannelForm = styled.form`
   display: flex;
@@ -88,6 +53,8 @@ const ChannelForm = styled.form`
   align-items: center;
   justify-content: center;
   width: 100%;
+
+  p { margin: 0 }
 `
 
 const ChannelInput = styled.input`
@@ -104,10 +71,11 @@ const ChannelInput = styled.input`
   }
 `
 
-const CreateButton = styled.label`
+const CreateButton = styled.input`
   padding: 5px 10px;
   background-color: black;
   color: white;
+  border: none;
   border-radius: 10px;
   cursor: pointer;
   transition: 0.3s;
@@ -117,33 +85,6 @@ const CreateButton = styled.label`
     color: black;
     border-radius: 5px;
     transform: scale(1.1);
-  }
-`
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  height: 30px;
-  width: 30px;
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  font-size: 1.6rem;
-  color: #000;
-  transition: 0.3s;
-  padding: 0;
-  border-radius: 50%;
-
-  svg {
-    height: 30px;
-    width: 30px;
-  }
-
-  &:hover {
-    transform: scale(1.1);
-    background-color: #f0f0f0;
-    color: #909090;
   }
 `
 
